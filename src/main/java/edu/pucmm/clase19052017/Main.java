4e253700f0c7670d7198636369d3ff4113a65b29
vacax <ccamachog@avathartech.com>
2017-05-19 11:44:41 -0400

    Cambios del primer día de clase.

---
+++ src/main/java/edu/pucmm/clase19052017/Main.java
@@ -0,0 +1,12 @@
+package edu.pucmm.clase19052017;
+
+/**
+ * Created by vacax on 19/05/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo en Java :-D");
+    }
+
+}
